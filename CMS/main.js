b4c1c7fd5524b3ff01f3a17130173af84d8fd364
sampurcell93 <Samuel.Purcell@tufts.edu>
2012-10-10 15:29:42 -0400

    Add text at cursor, not newline

--- CMS/main.js
+++ CMS/main.js
@@ -29,8 +29,7 @@ $(document).ready(function() {
 			var pair = $(this).parent().attr("data-rel");
 			var pairVal = $("#" + pair).attr("value");
 			//append list item to textarea
-			$("#" + pair).attr("value", pairVal + 
-				$(this).attr("data-content") + "\n");
+			insertAtCaret(pair, $(this).attr("data-content"));
 		});
 
 
@@ -102,24 +101,44 @@ function newConsole(the_index) {
 
 function aggregate() { 
 
-	var textboxes = [];
-	var headers = [];
+	console.log($("#previewContent").html());
+	$("#passtext").attr("value",$("#previewContent").html());
+	$("#passtitle").attr("value",$("#node_title").val());
 
-	for (var i = 0; i < $("textarea").length; i++) { 
-
-
-		textboxes[i] = $("#text" + i).val();
-		console.log('text: ' + textboxes[i]);
+}
 
 
-	}
+//props to gclaghorn of stackoverflow for the awesome function
+function insertAtCaret(areaId,text) {
+    var txtarea = document.getElementById(areaId);
+    var scrollPos = txtarea.scrollTop;
+    var strPos = 0;
+    var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? 
+        "ff" : (document.selection ? "ie" : false ) );
+    if (br == "ie") { 
+        txtarea.focus();
+        var range = document.selection.createRange();
+        range.moveStart ('character', -txtarea.value.length);
+        strPos = range.text.length;
+    }
+    else if (br == "ff") strPos = txtarea.selectionStart;
 
-	for (var i = 0; i < $("input[type=text]").length - 1; i++) { 
-
-		headers[i] = $("#head" + i).val();
-		console.log('header: ' + headers[i]);
-
-	}
-
-
+    var front = (txtarea.value).substring(0,strPos);  
+    var back = (txtarea.value).substring(strPos,txtarea.value.length); 
+    txtarea.value=front+text+back;
+    strPos = strPos + text.length;
+    if (br == "ie") { 
+        txtarea.focus();
+        var range = document.selection.createRange();
+        range.moveStart ('character', -txtarea.value.length);
+        range.moveStart ('character', strPos);
+        range.moveEnd ('character', 0);
+        range.select();
+    }
+    else if (br == "ff") {
+        txtarea.selectionStart = strPos;
+        txtarea.selectionEnd = strPos;
+        txtarea.focus();
+    }
+    txtarea.scrollTop = scrollPos;
 }
