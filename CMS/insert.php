7078c1257d011b8d029ff01e626dd40a803776c6
Sam Purcell <Samuel.Purcell@tufts.edu>
2012-10-16 20:49:51 -0400

    preview as it exists in the db

--- CMS/insert.php
+++ CMS/insert.php
@@ -1,14 +1,28 @@
-<!doctype html>
-	<html>
+<!DOCTYPE HTML>
+	<html lang="en">
 		<head>
-			
-			 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+			<link rel="stylesheet" media="screen" type="text/css" href="css/style.css" />
+			<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+			<script src="http://code.jquery.com/jquery-1.8.1.min.js"></script>
+			<script src="main.js"></script>
 
- 	</head>
+			<title>Add Dental Content</title>
+		</head>
 
- 	<body>
+		<body>
 
 <?php
+
+	$mysqli = new mysqli("localhost","root","","dental_info");
+
+	if($mysqli->connect_errno) { 
+
+		echo $mysqli->connect_error;
+		exit();
+
+	}
+
 	//get title of table
 	$title = $_POST['title'];
 	$title = utf8_encode($title);
@@ -24,25 +38,26 @@
 	$headers = explode("|",$headers);
 
 	//create a table creation string which dynamically loads all of the headers into new columns
-	$table_create_string = "CREATE TABLE $title (id int NOT NULL AUTO_INCREMENT, Primary Key(id), ";
+	$table_create_string = "CREATE TABLE $title (";
 
-		for($i = 0; $i < count($headers); $i++) { 
+	for($i = 0; $i < count($headers); $i++) { 
 
-				$headers[$i] = str_replace(" ","_",$headers[$i]);
+			$headers[$i] = str_replace(" ","_",$headers[$i]);
 
-				if($i > 0) { 
-					$headers[$i] = substr($headers[$i], 1, strlen($headers[$i]));
-				}
+			if($i > 0) { 
+				$headers[$i] = substr($headers[$i], 1, strlen($headers[$i]));
+			}
 
-				$table_create_string .= $headers[$i] . " text, ";
+			$table_create_string .= $headers[$i] . " text, ";
 		
-		}
+	}
 
-		$table_create_string = substr($table_create_string,0,-2);
-		$table_create_string .= ")";
-		echo $table_create_string . "\n";
+	$table_create_string = substr($table_create_string,0,-2);
+	$table_create_string .= ")";
 
-	$content_addition = "INSERT INTO $title VALUES('',";
+	$mysqli->query($table_create_string);
+
+	$content_addition = "INSERT INTO $title VALUES(";
 
 		for($i = 0; $i < count($text); $i++) { 
 
@@ -54,12 +69,7 @@
 
 	$content_addition  = substr($content_addition, 0, -2);
 	$content_addition .= ")";
-	$content_addition = htmlspecialchars($content_addition);
-
-	echo $content_addition;
-
-	$mysqli = new mysqli("localhost","root","","dental_info");
-
+	//$content_addition  = htmlspecialchars($content_addition);
 	
 	if($mysqli->connect_errno) { 
 
@@ -75,19 +85,41 @@
 
 	$results = $mysqli->query($query);
 
-	while($row = $results->fetch_assoc()) {	
-
-		foreach($row as $k=> $v) { 
-
-
-				echo $v;
-			
-
-		}
-	}
-
 	mail("spurcell93@gmail.com","prof mufte added a node!","Get it!");
+
+	echo "<div id='lander_wrap'><p style='text-align:center'>Your submission was successful! <br />";
+	echo "<a href='index.php' class='button'>Go back</a>";
+
 	?>
+
+	<a id="showPreview" href="#">See what you created, as it exists in our databases.</a>
+
+	<div id="dbPreview" style="display: none; ">
+
+
+		<?php
+
+			while ($row = $results->fetch_assoc()) { 
+
+
+				foreach($row as $k=>$v) { 
+
+
+					echo "<h2>" . str_replace("_"," ",$k) . "</h2>";
+					echo $v;
+
+
+				}
+
+			}
+
+
+
+			?>
+
+	</div>
+
+</div>
 
 </body>
 </html>
